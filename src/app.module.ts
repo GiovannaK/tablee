d2e81ed361462261075ca2156553e0cbf4b073a3
Giovanna Cunha <giovannakelli@gmail.com>
2022-03-19 18:04:35 -0300

    feat(TAB-11): Intial setup was created

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppResolver } from './app.resolver';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, AppResolver],
+})
+export class AppModule {}
