c27e2581af3f77ab62b1097c08f0a00175a5758b
Giovanna Cunha <giovannakelli@gmail.com>
2022-03-26 19:25:19 -0300

    fix: dockerfile was ficed

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:16 as development
+FROM node:16
 
 RUN mkdir -p /usr/src/app
 
@@ -10,31 +10,8 @@ RUN npm install
 
 COPY . .
 
-EXPOSE 4000
+EXPOSE 3001
 
 RUN npm run build
 
-
-FROM node:16 as production
-
-ARG NODE_ENV=production
-
-ENV NODE_ENV=${NODE_ENV}
-
-RUN mkdir -p /usr/src/app
-
-WORKDIR /usr/src/app
-
-COPY package*.json ./
-
-RUN npm install --only=production
-
-COPY . .
-
-COPY --from=development /usr/src/app/dist ./dist
-
-EXPOSE 4000
-
-RUN npm run build
-
-CMD [ "npm", "run", "start:dev" ]
+CMD [ "node", "dist/main" ]
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,76 +1,76 @@
-import { Field, ID, ObjectType,  } from '@nestjs/graphql';
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
-import { Role } from './role/user.role';
+// import { Field, ID, ObjectType,  } from '@nestjs/graphql';
+// import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+// import { Role } from './role/user.role';
 
-@ObjectType()
-@Entity()
-export class User {
-  @PrimaryGeneratedColumn('uuid')
-  @Field(() => ID)
-  id: string;
+// @ObjectType()
+// @Entity()
+// export class User {
+//   @PrimaryGeneratedColumn('uuid')
+//   @Field(() => ID)
+//   id: string;
 
-  @Column({
-    length: 200,
-    nullable: false,
-  })
-  firstName: string;
+//   @Column({
+//     length: 200,
+//     nullable: false,
+//   })
+//   firstName: string;
 
-  @Column({
-    length: 200,
-    nullable: false,
-  })
-  lastName: string;
+//   @Column({
+//     length: 200,
+//     nullable: false,
+//   })
+//   lastName: string;
 
-  @Column({
-    length: 320,
-    unique: true,
-    nullable: false,
-  })
-  email: string;
+//   @Column({
+//     length: 320,
+//     unique: true,
+//     nullable: false,
+//   })
+//   email: string;
 
-  @Column({
-    length: 11,
-    nullable: false,
-  })
-  mainPhone: string;
+//   @Column({
+//     length: 11,
+//     nullable: false,
+//   })
+//   mainPhone: string;
 
-  @Column({
-    length: 11,
-    nullable: true,
-  })
-  secondaryPhone: string;
+//   @Column({
+//     length: 11,
+//     nullable: true,
+//   })
+//   secondaryPhone: string;
 
-  @Column({ default: false })
-  isRegisteredWithGoogle: boolean;
+//   @Column({ default: false })
+//   isRegisteredWithGoogle: boolean;
 
-  @Column({
-    type: 'enum',
-    enum: Role,
-    default: Role.USER,
-  })
-  role: Role;
+//   @Column({
+//     type: 'enum',
+//     enum: Role,
+//     default: Role.USER,
+//   })
+//   role: Role;
 
-  @Column({
-    nullable: true,
-  })
-  loginToken: string;
+//   @Column({
+//     nullable: true,
+//   })
+//   loginToken: string;
 
-  @Column({
-    nullable: true,
-  })
-  expirationLoginToken: string;
+//   @Column({
+//     nullable: true,
+//   })
+//   expirationLoginToken: string;
 
-  @Column({
-    nullable: true,
-  })
-  stripeCustomerId: string;
+//   @Column({
+//     nullable: true,
+//   })
+//   stripeCustomerId: string;
 
-  @DeleteDateColumn()
-  deletedAt: Date;
+//   @DeleteDateColumn()
+//   deletedAt: Date;
 
-  @CreateDateColumn()
-  createdAt: Date;
+//   @CreateDateColumn()
+//   createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
-}
+//   @UpdateDateColumn()
+//   updatedAt: Date;
+// }
